2e9867fa43e91080221114c04b95aee12d04a63a
cogito17 <729426@student.dusd.net>
2024-09-25 11:22:45 -0700

    completed scripting assignment
    Completed my first scripting assignment

---
+++ ScriptingProjectP4Vargas/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+        }
+    }
+}
